9846abeb062f186fee1d3dd2255ad480cfc6c0af
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-16 12:57:26 -0700

    size of different data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+void main()
+{
+clrscr();
+printf("TypettttSize (bytes)");
+printf("nIntegertttt %d",sizeof(char));
+printf("nIntegertttt %d",sizeof(long int));
+printf("nFloatttt %d",sizeof(double));
+printf("nlong doublettt %d",sizeof(long double));
+getch();
+return (0);
+}
