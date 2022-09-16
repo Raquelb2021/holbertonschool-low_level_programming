6764c754ae0bed7266ed6d580dbf7982247c977a
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-16 10:18:05 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
-{	//string initialisation
-	char Mystr[] = "Programming is like building a multilingual puzzle;
+
+{	char Mystr[] = "Programming is like building a multilingual puzzle;
 
 	puts(Mystr); 
 
