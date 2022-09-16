baf4ddfc72c795164e8c6b9c714bfced1af1db9d
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-16 09:13:04 -0700

    Printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#!/bin/bash
+#include <stdio.h>
+int main() {
+	/* printf()  Displays teh string "Programming is like building a multilingual puzzle" */
+    	printf("Programming is like building a multilingual puzzle");
+	return 0;
+}
