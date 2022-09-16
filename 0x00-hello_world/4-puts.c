93baa97833c0f88d1043c765ea9240cb5be31046
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-16 11:54:20 -0700

    Using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /* C program that prints a phrase in the screem */
 int main(void)
 {
-puts(""Programming is like building a multilingual puzzle\n"");
+puts"("Programming is like building a multilingual puzzle\n")";
 return (0);
 }
