17b3e436c378acb4f1a212b6fb9774be35da59b4
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-16 11:51:43 -0700

    Using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,6 @@
 /* C program that prints a phrase in the screem */
 int main(void)
 {
-/* Using puts */
-puts("Programming is like building a multilingual puzzle\n");
+puts(""Programming is like building a multilingual puzzle\n");
 return (0);
 }
