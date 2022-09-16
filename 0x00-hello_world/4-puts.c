7938bf23683f8eaa56399d1b8a19f896faefbfd4
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-16 10:40:18 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Description: 'the program's description'
- *
- * return: Always 0 
- */ 
+
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
