a565b54418ba4e520415761661f4799a4fee2460
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-16 12:30:25 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,9 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- * */
-int main(void); {
+ */
+int main(void);
+{
 	printf("with proper gramar, but the outcome is a piece of art,");
 	return (0);
 }
