934b7f778026f5b6b5a9c1e479151c593e3a2381
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-16 13:28:52 -0700

    size of different data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,16 +5,17 @@
  *
  * Return: Always 0 (Sucess)
  */
-int main(void) {
+int main(void)
+{
 	int intType;
 	float floatType;
 	double doubleType;
 	char charType;
 	
-	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of double: %zu bytes\n", sizeof(doubleTypes));
-	printf("Size of char: %zu byte\n", sizeof(charType));
+	printf("Size of int: %d bytes\n", sizeof(intType));
+	printf("Size of float: %d bytes\n", sizeof(floatType));
+	printf("Size of double: %d bytes\n", sizeof(doubleTypes));
+	printf("Size of char: %d byte\n", sizeof(charType));
 
 	return (0);
 }
