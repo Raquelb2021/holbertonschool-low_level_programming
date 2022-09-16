554dabd3628d06a9bd5eba474db573a4cb13c31f
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-16 10:26:19 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
-#include<stdio.h>
-int main()
-{
-puts("Programming is like building a multilingual puzzle");
+#include <stdio.h>
 
-return 0;
+main()
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return 0;
 }
