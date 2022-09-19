950692ee8707acf63a10ddb19f4f32b493c3ab17
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-19 11:09:58 -0700

    lowercase alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,8 +6,10 @@
  */
 int main(void)
 {
-	int ch;
-	for (ch= 'a'; ch<= 'z'; ch++)
+	char ch;
+
+	printf("\nLowercase characers : \n");
+	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 		putchar ('\n') ;
 		getch();
