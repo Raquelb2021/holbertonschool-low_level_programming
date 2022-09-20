e30057e13446f98f5b8a3da74cc44b6be4cf36a2
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-19 19:14:26 -0700

    while alpha

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,7 +8,14 @@ int main(void)
 char ch = '`';
 
 	while (ch < 'z')
+	{
+		ch++;
+		if (ch == 'e')
+			continue;
+		if (ch == 'q')
+			continue;
 	putchar(ch);
-putchar('\n');
+	}
+	putchar('\n');
 return (0);
 }
