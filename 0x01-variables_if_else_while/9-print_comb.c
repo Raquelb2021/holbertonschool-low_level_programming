ccf3833f38adb711e42eb762e613099aad0680e4
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-20 10:45:41 -0700

    hexazonal using putcharq

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,13 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 9; num++)
+	for (num = 0; num < 10; num++)
 
 	putchar((num % 10) + '0');
 
 	putchar(',');
 	putchar(' ');
+
 	putchar('\n');
 
 	return (0);
