7d853e70feb9d8a38d413494b2e32928ea20546f
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-20 09:00:13 -0700

    Number 0 to 9 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Prints all numbers of base 10 starting from 0 using putchar
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	putchar((num % 9) + '0');
+	putchar('\n');
+	return (0);
+}
