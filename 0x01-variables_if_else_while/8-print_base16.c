78cd527a49161e777aa551f4b38f441c2ad5159d
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-20 10:03:41 -0700

    hexazonal using putchar

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ *  main - hexazonal using putchar
+ * 
+ *  Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	char ch;
+	
+	for (num = 0; num <= 9; ch++)
+	putchar(num % 10) + 0);
+
+	for (num = 'a'; num <= 'f'; ch++)
+	putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
