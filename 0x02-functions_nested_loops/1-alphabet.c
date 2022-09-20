682a96308cd9de3c2a9975c86f05ce1d3d44db2e
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-20 15:11:22 -0700

    prints alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,15 +3,12 @@
  * main - entry point
  * Return: 0
  */
-int main(void)
-{
 	void print_alphabet(void);
-
+{
 		char ch;
 
 		for (ch = 'a'; ch <= 'z'; ch++)
 			_putchar(ch);
 
 		_putchar('\n');
-		return (0);
 }
