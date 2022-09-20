d7a3f733b7185ec1e6e09f243ef31bc5294f30c8
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-20 16:49:52 -0700

    prints alphabet in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+/**
+ * File 1-alphabet.c
+ * Auth: Raquel Bezerra
+ */
+int main(void)
+
+/**
+ * print_alphabet - print alphabet in lowercase 10 times
+ *
+ * return:0(success)
+ */
+	void print_alphabet_x10(void);
