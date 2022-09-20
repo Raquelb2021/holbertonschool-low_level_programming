aa702fb0fbe8bcd36ae40a69f7f21ca3da16853d
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-20 15:57:12 -0700

    prints alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,13 +6,13 @@
 #include "main.h"
 
 /**
- *  * function_name - Short description, single line
- *   * @parameterx: Description of parameter x
- *   (* a blank line
- *    * Description: Longer description of the function)?
- *    (* section header: Section description)*
- *     * Return: Description of the returned value
- *      */
+ * function_name - Short description, single line
+ * @parameterx: Description of parameter x
+ * a blank line
+ * Description: Longer description of the function)?
+ * (* section header: Section description)*
+ * Return: Description of the returned value
+ */
 void print_alphabet(void)
 {
 		char letter;
