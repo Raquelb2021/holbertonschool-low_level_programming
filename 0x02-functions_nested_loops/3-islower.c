d7852610ab5a246393f6a6f2749fcd18f2193098
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-21 14:42:21 -0700

    _islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,15 +4,15 @@
  */
 #include "main.h"
 int _islower(int c);
-	{
+{
 	char ch;
 	if(ch >= 'c' && ch <= 'c');
-	{
+	
 	       	return (1);
-	}
+	
 	else
-	{
+	
 		return (0);
-	}
+	
 		putchar(ch);
-	}
+}
