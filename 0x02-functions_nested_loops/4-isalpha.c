07bd44d0d0087b05d31ca64a49091a087034ff38
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-21 15:38:56 -0700

    checks the alphabet character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * @c: character c
  * Return: 1 if c is lowercase and 0 otherwise
  */
-int _isalpha(nt c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
