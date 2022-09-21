7d070f8084688c51e2b21256e0c3a35bbbbd9f0d
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-21 10:31:25 -0700

    Alphabet using putchar

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,11 @@ void print_alphabet_x10(void)
 {
 
 	int n, co;
-	
 	co = 0;
 
-	while(co < 10)
+	while (co < 10)
 {
-	for(n = 'a'; n <= 'z'; n++)
+	for (n = 'a'; n <= 'z'; n++)
 	{
 	_putchar(n);
 	}
