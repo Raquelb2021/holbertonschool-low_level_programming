2e80d5b8ded8840a74ff3475f49763f5e2d3f100
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-21 12:53:12 -0700

    checks lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+/**
+ * int _islower(int c); - checks for lowercase character
+ * Return: Always 0.
+ */
+#include "main.h"
+int _islower(int c);
+{
+	char ch;
+	if(ch >= 'c' && ch <= 'c')
+	{
+	       	return (1);
+	}
+	else
+	{
+		return (0);
+	}
+		putchar(ch)
+}
