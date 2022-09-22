8d54fb157fb73f94dda0fc8d7e88e8f84094f818
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-22 11:22:22 -0700

    print the numbers 0 to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,17 +5,18 @@
  */
 void print_to_98(int n);
 {
-	if(n >= 0)
-	{
-		while (n > 98);
+        if(n >= 0)
+        {
+
+	        while (n > 98);
 			printf("%d ",n--);
 		printf9"%d\n", n);
-	}
-	
-	else
-	{
-		while 9n < 98)
-			printf("%d, ", n++);
-		printf("%d\n", n);
-	}
+  	}
+
+        else
+        {
+                while (n < 98)
+			 printf("%d, ", n++);
+                printf("%d\n", n);
+       }
 }
