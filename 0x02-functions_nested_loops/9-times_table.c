978b5c491bb5d52d061d0ee66627cbb2e032e459
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-22 09:35:18 -0700

    display the 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * time_table - prints the 9 times table
+ * Return: Always 0
+ *
+ */
+void times_table(void);
+{
+	int num, mult, result;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+	for (mult = 1; mult <= 9; mult++)
+	{
+		_putchar(',');
+		_putchar(' ');
+
+		if (result <= 9)
+			_putchar(' ');
+		else
+			_putchar((result / 10) + '0');
+
+		_putchar((result % 10) + '0');
+	}
+	_putchar('\n');
+	}
+}
+
+
