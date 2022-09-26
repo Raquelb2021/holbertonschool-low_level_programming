5b5fa67d911c1f456656591f67f1e887326fdabe
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-26 09:31:19 -0700

    Prints the numbers 0 to 14, 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * more_numbers - Prints 10 times, from 0 to 14.
+ * n - the variable
+ *
+ */
+void more_numbers(void)
+{
+	int n;
+	{
+	for (n = 0; n <= 14; n++)
+	{
+		_putchar(n + '0');
+	}
+
+	}
+	_putchar('\n');
+}
