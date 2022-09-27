ed957e242d48a8e0ff1ba1968e16e5ac52dfd48b
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-27 14:00:15 -0700

    returns the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,10 +9,10 @@
  */
 int _strlen(char *s)
 {
-	int count;
+	 int c = 0;
 
-	for (count = 0; s[count] != '\0'; ++count)
+	   while (s[c] != '\0')
+	    	   c++;
 
-
-	return (count);
+	       return (c);
 }
