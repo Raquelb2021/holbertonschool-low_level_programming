c0ceab47cd94763701f50c0a3343f9a6fc9327d6
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-26 17:43:46 -0700

    Draws a diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n > 0)
+	{
+		for (x = 0; x < n; x++)
+		{
+		
+		for (y = 0; y < n; y++)
+		
+		_putchar(' ');
+
+		_putchar('\\');
+
+
+		}
+	}
+	_putchar('\n');
+}		
