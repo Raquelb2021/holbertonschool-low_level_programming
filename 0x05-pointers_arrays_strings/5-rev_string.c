28961e9ad5e544bdcda23c9d922c33e12e048f8d
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-27 20:12:15 -0700

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,7 +10,7 @@ void rev_string(char *s)
 	int length = strlen(s);
 	int a, b, c;
 
-	for (b = 0, c = length - 1; a; b++, c--)
+	for (b = 0, c = length - 1; b < c; b++, c--)
 	{
 		a = s[b];
 		s[b] = s[c];
