22248cc5a6032e7abef2d7ac0ccbec49c5de3abd
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-27 20:38:16 -0700

    print every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,12 @@ void puts2(char *str)
 	int prints = 0, a = 0;
 
 	while (str[prints++])
-	
+
 		a++;
 
 	for (prints = 0; prints < a; prints += 2)
 
 		_putchar(str[prints]);
-	
+
 	_putchar('\n');
 }
