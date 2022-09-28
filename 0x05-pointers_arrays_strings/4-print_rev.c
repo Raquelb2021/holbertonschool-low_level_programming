4a11cb9f5c12f7e7c26fe0ed37aa3c38056cb1c1
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-27 18:43:50 -0700

    Prints a string, in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,17 +6,16 @@
  */
 void print_rev(char *s)
 {
-	int a = 0;
-		
-		while (*(s + a))
-		
-			a++;
-		a = a - 1;
+	int a = 0, b;
 
-		while (a >= 0)
-		{
-			_putchar(*(s + 1));
-			a--;
-		}
-		_putchar('\n');
+	while (s[b++])
+	{
+		a++;
+
+	for (b = a - 1; b >= 0; b--)
+
+		_putchar(s[b]);
+	}
+
+	_putchar('\n');
 }
