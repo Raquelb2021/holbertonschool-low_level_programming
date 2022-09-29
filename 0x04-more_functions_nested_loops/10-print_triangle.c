e7e35108f38189b1b0bee4a0d7ea7104395430fb
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-28 18:27:54 -0700

    print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,16 +9,18 @@ void print_triangle(int size)
 	int i, a;
 
 	if (size <= 0)
-	{
+	
 	_putchar('\n');
-	}
-	else
 
-
-	for (i = 0; i < size; i++)
+	for (i = 1; i < size; i++)
 	{
-		for (a = 0; a <= i; a++)
+		for (a = 1; a <= i; a++)
 		{
+			if (a <= (size - a))
+
+		_putchar(' ');
+
+		else
 
 		_putchar('#');
 		}
