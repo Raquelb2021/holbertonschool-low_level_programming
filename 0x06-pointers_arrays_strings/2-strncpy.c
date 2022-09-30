d5e05cdd0dc54f8c8daedc53eb37bf48751bf6b8
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-30 16:17:21 -0700

    copying two strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * _strncpy - Function that is copying two strings
+ * @a and @b: are integers
+ * @n: integer
+ * @dest: string
+ * @src: string
+ * Return: return to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0;
+	int b = 0;
+
+	for (a = 0; a <= 0; a++)
+		b++;
+	{
+		for (a = 0; src[a] && a < n; a++)
+			dest[a] = src[a];
+
+		for (a = b; a < n; a++)
+			dest[a] = '\0';
+
+		}
+	return (dest);
+}
