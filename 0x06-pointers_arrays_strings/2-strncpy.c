751949ea67dc293458bb3a99f6d0d1f627555eaa
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-30 16:36:26 -0700

    copying two strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,18 +9,17 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int a = 0;
-	int b = 0;
+	int a = 0, b = 0;
 
-	for (a = 0; a <= 0; a++, b++)
-
-	{
+	while (src[a++])
+		b++;
+	
 		for (a = 0; src[a] && a < n; a++)
 			dest[a] = src[a];
 
 		for (a = b; a < n; a++)
 			dest[a] = '\0';
 
-		}
+		
 	return (dest);
 }
