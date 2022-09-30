fd31fa0bf5d10de7c001c4a11b711e06e5c181f6
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-09-30 11:09:43 -0700

    strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _strncat - function that in joing 2 strings.
+ * @dest: string 
+ * @src: string
+ * Return: Resturn dest
+ * @n: integer
+ *
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a = 0, b = 0;
+
+	while (dest[a++])
+		b++;
+
+	for (a = 0; src[a] && a < n; a++)
+		dest[b++] = src[a];
+
+ return (dest);
+}
