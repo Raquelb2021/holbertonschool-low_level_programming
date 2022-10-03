f4f1425e982f053f1e7c30aff41dc71662323073
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-10-03 07:27:41 -0700

    capitalizes word of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,21 +5,45 @@
 char *cap_string(char *s)
 {
 	int a;
-	int b;
 
 
-	while (s[a] != '\0')
+	for (a = 0; s[a] != '\0'; a++)
 	{
-		if ((b %2 == 1) && s[a] >= 'a' && s[a] <= 'z')
+		if (a == 0)
 		{
-			s[a] = s[a] - 32;
-		}
-		else if (s[a] == 32 || 10 || 9 || 44 || 59 || 46 || 33 || 63 || 34 || 40 || 41 || 123 || 125)
-		{
-			b++;
+			if ((s[a] >= 'a' && s[a] <= 'z'))
+				s[a] = s[a] - 32;
+			continue;
 		}
 
-	a++;
-	}	
+		switch (s[a])
+		{
+			case ' ':
+			case '\t':
+			case '\n':
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+
+			a++;
+			if (s[a] >= 'a' && s[a] <= 'z')
+			{
+				s[a] = s[a] - 32;
+				continue;
+			}
+			
+			break;
+
+			default:
+			break;
+		}
+	}
 	return (s);
 }
