2dfbed8270fcbfc9b0d2c2416f800baf74ee4dbd
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-10-02 18:44:12 -0700

    capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,14 +7,18 @@ char *cap_string(char *s)
 	int a;
 	int b;
 
-	for (b = 0; s[b] != '\0'; b++)
 
 	while (s[a] != '\0')
 	{
-		if (s[a] >= 'a' && s[a] <= 'z')
+		if ((b %2 == 1) && s[a] >= 'a' && s[a] <= 'z')
 		{
 			s[a] = s[a] - 32;
 		}
+		else if (s[a] == 32 || 10 || 9 || 44 || 59 || 46 || 33 || 63 || 34 || 40 || 41 || 123 || 125)
+		{
+			b++;
+		}
+
 	a++;
 	}	
 	return (s);
