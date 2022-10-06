f33d792a06f08e04503517415260191c93061eb4
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-10-05 19:28:52 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,27 +3,29 @@
  * times_table - prints the 9 times table
  */
 void times_table(void)
-
 {
 	int num, mult, result;
+
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
+	}
 
-		for (mult = 1; mult <= 9; mult++)
-		{
-			_putchar(',');
+	for (mult = 0; mult <= 9; mult++)
+	{
+		_putchar(' ');
+		_putchar(',');
+
+		result = num * mult;
+
+		if (result <= 9)
 			_putchar(' ');
 
-			result = num * mult;
+		else
+			_putchar((result / 10) + '0');
 
-			if (result <= 9)
-				_putchar(' ');
-			else
-				_putchar((result / 10) + '0');
+		_putchar((result % 10) + '0');
 
-			_putchar((result % 10) + '0');
-		}
-		_putchar('\n');
 	}
-	}
+	_putchar('\n');
+}
