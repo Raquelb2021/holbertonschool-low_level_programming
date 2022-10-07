04fa8047a388cf59a47169716c3a725919f8b999
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-10-07 12:58:47 -0700

    string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,16 @@
 #include "main.h"
+#include <string.h>
 /**
- *
+ * _print_rev_recursion - Prototype that prints a string in reverse
+ * @s: string
  *
  */
 void _print_rev_recursion(char *s)
 {
+	if (*s != '\0')
+	{
+	_print_rev_recursion(s + 1);
 
+		_putchar(*s);
+	}
+}
