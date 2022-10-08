fc5272965b73a874a722f543d677845bf6903b89
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-10-08 11:52:58 -0700

    return value the x raised to the power of y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
- *
- *
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: this is the number base
+ * @y: number raised in x
+ * Return: return value of x
  */
 int _pow_recursion(int x, int y)
 {
-	if (y == 0 && x == 0)
-	{
-		return(1);
-	}
+	if (y < 0)
+		return (-1);
 
-	if (y > 0)
-	{
-		return (x * _pow_recursion(x, y -1));
-	}
-	return(1);
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
 }
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0 || n == 1)
+	{
+		return (-1);
+	}
+	if (n && n)
+	{
+	return (n);
+	}
+	return (1);
+}
