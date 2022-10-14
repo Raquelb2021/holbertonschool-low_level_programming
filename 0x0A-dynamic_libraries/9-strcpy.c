b6172e78bfa191e1b18fcaa4a74edfc4c55cceb4
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-10-14 09:00:52 -0700

    create a dynamic library

---
+++ 0x0A-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+ * _strcpy - copies the string pointed
+ * @dest: ths is the pointer to the destination array
+ * @src: this is the string to be copied
+ * Return: 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (0);
+}
