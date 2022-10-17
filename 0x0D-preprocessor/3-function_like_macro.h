4e2d39ace57c28c3b4a47bf680de2820966805f4
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-10-17 12:47:30 -0700

    computes the absolute value of a numberx

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,8 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
-#include <stdlib.h>
 
-#define ABS(x) ((x <= 0) ? (-x) : (x))
+
+
+#define ABS(x) ((x < 0) ? (x) * (-1) : (x))
 
 #endif
