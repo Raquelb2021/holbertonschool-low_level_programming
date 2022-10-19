e293e41da970bb06d99e2e303d914c9c1f3de502
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-10-18 19:50:26 -0700

    allocaes memory using malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,4 +7,15 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	char *s;
+	int space_needed;
 
+	space_needed = strlen(s1) + strlen(s2) + 1
+
+	s = malloc(space_needed);
+
+	strcpy(s, s1);
+	strcpy(s + strlen(s1), (s2);
+
+			return (n);
+}
