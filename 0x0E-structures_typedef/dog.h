f7549e23791c65bea3bc51d3ef27fdbc359de466
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-10-20 13:05:09 -0700

    contem my prototypesand the new type "struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,11 +14,11 @@ struct dog
 	};
 
 /**
- * my_dog - Typedef for struct dog
+ * dog_t - Typedef for struct dog
  *
  */
-typedef struct dog my_dog;
 
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
